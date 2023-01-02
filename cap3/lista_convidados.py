a83bf8855d1f39eb3d7f7b664533368f3016cff4
Wesley Oliveira <westen.py@gmail.com>
2023-01-02 20:55:42 -0300

    Create lista_convidados.py

---
+++ cap3/lista_convidados.py
@@ -0,0 +1,10 @@
+#Crie uma lista que convide ao menos três pessoas para jantar.
+#Em seguida utilize sua lista para exibir uma mensagem para cada uma
+#convidando-a para jantar.
+
+pessoas = ['Lud', 'Vita', 'Jão', 'Bumba']
+
+print(f'{pessoas[0]} gostaria de jantar comigo?\n')
+print(f'{pessoas[1]} gostaria de jantar comigo?\n')
+print(f'{pessoas[2]} gostaria de jantar comigo?\n')
+print(f'{pessoas[3]} gostaria de jantar comigo?\n')
