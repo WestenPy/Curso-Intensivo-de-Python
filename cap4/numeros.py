d3109d116ab498e8e3f494fefccf2a0dc68cf0d9
Wesley Oliveira <westen.py@gmail.com>
2023-01-11 22:46:22 -0300

    linha

---
+++ cap4/numeros.py
@@ -0,0 +1,8 @@
+#Usando afunção range()
+for valor in range(1, 5):
+    print(valor)
+print()
+
+#Para exibir os números de 1 a 5, você deve usar range(1,6):
+for valor in range(1, 6):
+    print(valor)
---
+++ cap4/numeros_lista.py
@@ -0,0 +1,8 @@
+#Usando range() para criar uma lista de números
+numeros = list(range(1, 6))
+print(numeros)
+print()
+
+#podemos criar uma lista de determinada forma. Nesse caso, pulando de 2 em 2.
+numeros_pares = list(range(2, 11, 2))
+print(numeros_pares)
